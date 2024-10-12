f7f410bc0f00a2199f7706e9673e9d0f6f63d469
jeongmyeong <kimbym23@gmail.com>
2024-10-12 20:47:07 +0900

    create service test

--- backend/src/main/kotlin/com/photograph/backend/member/infra/MemberDocument.kt
+++ backend/src/main/kotlin/com/photograph/backend/member/infra/MemberDocument.kt
@@ -3,11 +3,13 @@ package com.photograph.backend.member.infra
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
 
-@Document
+@Document(value = "member")
 data class MemberDocument(
     @Id
     val memberId: String = ObjectId().toString(),
     val name: String,
     val email: String,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
 )
---
+++ backend/src/test/kotlin/com/photograph/backend/member/domain/MemberServiceTest.kt
@@ -0,0 +1,40 @@
+package com.photograph.backend.member.domain
+
+import com.photograph.backend.member.infra.MemberDocument
+import com.photograph.backend.member.infra.MemberRepository
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+class MemberServiceTest : DescribeSpec({
+
+    val memberRepository: MemberRepository = mockk()
+    val entity: MemberDocument = mockk(relaxed = true)
+    val member = Member("정명", "jeongmyeong@gmail.com")
+
+    describe("회원정보 서비스는") {
+        val service = MemberService(memberRepository)
+
+        context("회원정보가 있으면") {
+            every { memberRepository.findByName(any()) } returns entity
+
+            service.merge(member)
+
+            it("그대로 반환한다") {
+                verify(exactly = 0) { memberRepository.save(any()) }
+            }
+        }
+
+        context("회원정보가 없으면") {
+            every { memberRepository.findByName(any()) } returns null
+            every { memberRepository.save(any()) } returns entity
+
+            service.merge(member)
+
+            it("회원정보를 새로 저장한다") {
+                verify(exactly = 1) { memberRepository.save(any()) }
+            }
+        }
+    }
+})
