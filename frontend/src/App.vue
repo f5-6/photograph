a2b1fd3be2122b478806e51e7a04b441c76fc428
jeongmyeong <kimbym23@gmail.com>
2024-10-13 20:28:46 +0900

    create default google login button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
 <script lang="ts" setup>
+const host = import.meta.env.VITE_API_SERVER_URL
+
+const loginUrl = host + '/oauth2/authorization/google'
 </script>
 
 <template>
   <div>
-
+    <a :href=loginUrl>
+      <!-- https://developers.google.com/identity/branding-guidelines -->
+      <img alt="login" src="./assets/google_login.svg">
+    </a>
   </div>
 </template>
 
