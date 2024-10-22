588c735fb7ecddefbfebf2438b9dc218d5ee074f
jeongmyeong <kimbym23@gmail.com>
2024-10-22 21:31:38 +0900

    remove useless code, depend on application.yml servlet option

--- backend/src/main/kotlin/com/photograph/backend/config/security/component/AuthenticationSuccessHandler.kt
+++ backend/src/main/kotlin/com/photograph/backend/config/security/component/AuthenticationSuccessHandler.kt
@@ -1,6 +1,5 @@
 package com.photograph.backend.config.security.component
 
-import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Value
@@ -12,16 +11,12 @@ import org.springframework.stereotype.Component
 class AuthenticationSuccessHandler(
     @Value("\${frontend.url}") private val frontendUrl: String
 ) : SimpleUrlAuthenticationSuccessHandler() {
+
     override fun onAuthenticationSuccess(
-        request: HttpServletRequest?,
-        response: HttpServletResponse?,
-        authentication: Authentication?
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication
     ) {
-        response?.addCookie(
-            Cookie("JSESSIONID", request?.session?.id).apply {
-                maxAge = 60 * 5
-            }
-        )
         redirectStrategy.sendRedirect(
             request,
             response,
