cc61b3fb9eacdbb6cbc9690cdc7c6bcc2135c97e
quid-0 <wodnd101@naver.com>
2024-10-22 22:22:29 +0900

    /api 하위만 허용

--- backend/src/main/kotlin/com/photograph/backend/config/security/SecurityConfig.kt
+++ backend/src/main/kotlin/com/photograph/backend/config/security/SecurityConfig.kt
@@ -29,7 +29,7 @@ class SecurityConfig(
             }
             .authorizeHttpRequests { authorizeRequests ->
                 authorizeRequests
-                    .requestMatchers("/permit").permitAll()
+                    .requestMatchers("/api/**").permitAll()
                     .anyRequest().authenticated()
             }
             .exceptionHandling { it.authenticationEntryPoint(oauthAuthenticationEntryPoint) }
