04f4007f71e775f8039b849f77d701ae849779df
jeongmyeong <kimbym23@gmail.com>
2024-10-22 20:43:17 +0900

    apply vue3 grammar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,20 +3,10 @@
 </script>
 
 <template>
-  <div id="app">
-    <router-link to="/" class="nav">Home</router-link>
-    <router-link to="/test" class="nav">Test</router-link>
-    <router-view />
+  <div>
+    <router-view/>
   </div>
 </template>
 
 <style scoped>
-.nav {
-  margin: 10px;
-  padding: 10px;
-  font-size: 16px;
-  text-decoration: none;
-  color: #42b983;
-}
-
 </style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a :href=loginUrl>
+      <!-- https://developers.google.com/identity/branding-guidelines -->
+      <img alt="login" src="../assets/google_login.svg">
+    </a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {computed} from 'vue';
+
+const loginUrl = computed(() => `${import.meta.env.VITE_API_SERVER_URL}/oauth2/authorization/google`);
+</script>
+
+<style scoped>
+</style>
