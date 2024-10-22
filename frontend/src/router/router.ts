cd81e60d5e4102afe5d1477b12d498d5093f844f
jeongmyeong <kimbym23@gmail.com>
2024-10-22 21:34:09 +0900

    apply auth per page

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -12,6 +12,7 @@ const routes: Array<RouteRecordRaw> = [
         path: '/admin',
         name: 'Admin',
         component: Admin,
+        meta: {requiresAuth: true}
     }
 ];
 
@@ -19,5 +20,27 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 });
+
+router.beforeEach((to, from, next) => {
+    const isAuthenticated = !!getCookie('JSESSIONID');
+
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!isAuthenticated) {
+            next({name: 'Login'});
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+
+    return null;
+}
 
 export default router;
