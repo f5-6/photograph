4e8742a63d211fa7acba6c9096ff2f7c77b7903a
quid-0 <wodnd101@naver.com>
2024-10-23 16:48:15 +0900

    APPLICATION_JSON_VALUE 타입 적용

--- backend/src/main/kotlin/com/photograph/backend/config/security/component/OauthAuthenticationEntryPoint.kt
+++ backend/src/main/kotlin/com/photograph/backend/config/security/component/OauthAuthenticationEntryPoint.kt
@@ -2,6 +2,7 @@ package com.photograph.backend.config.security.component
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
@@ -15,7 +16,7 @@ class OauthAuthenticationEntryPoint : AuthenticationEntryPoint {
     ) {
         val errorMessage = authException?.message ?: "Unauthorized"
         response?.apply {
-            contentType = "application/json"
+            contentType = APPLICATION_JSON_VALUE
             status = HttpServletResponse.SC_UNAUTHORIZED
             writer.write("{\"message\": \"$errorMessage\"}")
         }
