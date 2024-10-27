b2802b117e1d61781abd5b642fbdb280e598e6dd
jeongmyeong <kimbym23@gmail.com>
2024-10-27 10:42:30 +0900

    vuetify install

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,9 @@ import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/router'
+import vuetify from "./plugins/vuetify.ts";
 
 createApp(App)
     .use(router)
+    .use(vuetify)
     .mount('#app')
